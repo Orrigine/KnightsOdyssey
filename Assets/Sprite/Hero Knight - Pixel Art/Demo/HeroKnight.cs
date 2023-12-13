a49d7da102453eb766dc1b52283151f5356d2b03
A. "Karma" Grandpaul <92379424+Karma192@users.noreply.github.com>
2023-12-13 16:03:25 +0100

    player can now do basics

--- Assets/Sprite/Hero Knight - Pixel Art/Demo/HeroKnight.cs
+++ Assets/Sprite/Hero Knight - Pixel Art/Demo/HeroKnight.cs
@@ -4,14 +4,13 @@ using System.Collections;
 public class HeroKnight : MonoBehaviour {
 
     [SerializeField] float      m_speed = 4.0f;
-    [SerializeField] float      m_jumpForce = 7.5f;
     [SerializeField] float      m_rollForce = 6.0f;
     [SerializeField] bool       m_noBlood = false;
     [SerializeField] GameObject m_slideDust;
+    [SerializeField] GameObject actionModule;
 
     private Animator            m_animator;
     private Rigidbody2D         m_body2d;
-    private bool                m_isWallSliding = false;
     private bool                m_rolling = false;
     private int                 m_facingDirection = 1;
     private int                 m_currentAttack = 0;
@@ -19,6 +18,12 @@ public class HeroKnight : MonoBehaviour {
     private float               m_delayToIdle = 0.0f;
     private float               m_rollDuration = 8.0f / 14.0f;
     private float               m_rollCurrentTime;
+
+    public int FacingDirection
+    {
+        get { return m_facingDirection; }
+        set { m_facingDirection = value; }
+    }
 
 
     // Use this for initialization
@@ -71,52 +76,26 @@ public class HeroKnight : MonoBehaviour {
 
         //Death
         if (Input.GetKeyDown("e") && !m_rolling)
-        {
-            m_animator.SetBool("noBlood", m_noBlood);
-            m_animator.SetTrigger("Death");
-        }
+            Die();
             
         //Hurt
         else if (Input.GetKeyDown("q") && !m_rolling)
-            
+            TakeDamage();
 
         //Attack
         else if(Input.GetMouseButtonDown(0) && m_timeSinceAttack > 0.25f && !m_rolling)
-        {
-            m_currentAttack++;
-
-            // Loop back to one after third attack
-            if (m_currentAttack > 3)
-                m_currentAttack = 1;
-
-            // Reset Attack combo if time since last attack is too large
-            if (m_timeSinceAttack > 1.0f)
-                m_currentAttack = 1;
-
-            // Call one of three attack animations "Attack1", "Attack2", "Attack3"
-            m_animator.SetTrigger("Attack" + m_currentAttack);
-
-            // Reset timer
-            m_timeSinceAttack = 0.0f;
-        }
+            Attack();
 
         // Block
         else if (Input.GetMouseButtonDown(1) && !m_rolling)
-        {
-            m_animator.SetTrigger("Block");
-            m_animator.SetBool("IdleBlock", true);
-        }
+            Block();
 
         else if (Input.GetMouseButtonUp(1))
             m_animator.SetBool("IdleBlock", false);
 
         // Roll
-        else if (Input.GetKeyDown("left shift") && !m_rolling && !m_isWallSliding)
-        {
-            m_rolling = true;
-            m_animator.SetTrigger("Roll");
-            m_body2d.velocity = new Vector2(m_facingDirection * m_rollForce, m_body2d.velocity.y);
-        }
+        else if (Input.GetKeyDown("left shift") && !m_rolling)
+            Roll();
 
         //Run
         else if (Mathf.Abs(inputX) > Mathf.Epsilon)
@@ -135,10 +114,63 @@ public class HeroKnight : MonoBehaviour {
                     m_animator.SetInteger("AnimState", 0);
         }
     }
+
+    private void Die()
+    {
+        m_animator.SetBool("noBlood", m_noBlood);
+        m_animator.SetTrigger("Death");
+    }
+
+    private void Attack()
+    {
+        m_currentAttack++;
+
+        // Loop back to one after third attack
+        if (m_currentAttack > 3)
+            m_currentAttack = 1;
+
+        // Reset Attack combo if time since last attack is too large
+        if (m_timeSinceAttack > 1.0f)
+            m_currentAttack = 1;
+
+        // Call one of three attack animations "Attack1", "Attack2", "Attack3"
+        m_animator.SetTrigger("Attack" + m_currentAttack);
+        float x = actionModule.GetComponent<Aiming>().Direction.x;
+        if (x > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+            m_facingDirection = 1;
+        }
+        if (x < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+            m_facingDirection = -1;
+        }
+    
+        Vector3 atkPos = actionModule.transform.position + actionModule.GetComponent<Aiming>().Direction;
+        atkPos.z = 0;
+        Quaternion atkRot = Quaternion.Euler(0, 0, Mathf.Atan2(atkPos.y, atkPos.x) * Mathf.Rad2Deg);
+        actionModule.GetComponent<PlayerAttack>().Attack(atkPos, atkRot);
+        
+        // Reset timer
+        m_timeSinceAttack = 0.0f;
+    }
     
     private void TakeDamage()
     {
         m_animator.SetTrigger("Hurt");
-        
+    }
+    
+    private void Block()
+    {
+        m_animator.SetTrigger("Block");
+        m_animator.SetBool("IdleBlock", true);
+    }
+
+    private void Roll()
+    {
+        m_rolling = true;
+        m_animator.SetTrigger("Roll");
+        m_body2d.velocity = new Vector2(m_facingDirection * m_rollForce, m_body2d.velocity.y);
     }
 }
