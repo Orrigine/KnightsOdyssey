31a8c717f4b9a716c85bf957138f44750ba78e40
Valentin Faguet <valentin@wallmatt.com>
2023-12-14 16:55:13 +0100

    :rainbow: Fix out of bounds agent

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -9,9 +9,12 @@ public class EnemyPatrol : MonoBehaviour
     private int posY;
     public bool Aggro = false;
 
+    NavMeshAgent _nav;
+
     // Start is called before the first frame update
     void Start()
     {
+        _nav = GetComponent<NavMeshAgent>();
         InvokeRepeating("Patrol", 0, 5);
     }
 
@@ -22,9 +25,31 @@ public class EnemyPatrol : MonoBehaviour
 
     public void Patrol()
     {
-        Debug.Log("Patrolling");
+        // patrol on a random range between -10 and 10 without going out of bounds
         posX = Random.Range(-10, 10);
         posY = Random.Range(-10, 10);
-        GetComponent<NavMeshAgent>().destination = new Vector3(posX + transform.position.x, posY + transform.position.y, 0);
+        if (posX > 10)
+        {
+            posX = 10;
+        }
+        else if (posX < -10)
+        {
+            posX = -10;
+        }
+        if (posY > 10)
+        {
+            posY = 10;
+        }
+        else if (posY < -10)
+        {
+            posY = -10;
+        }
+        Vector3 destination = new Vector3(posX + transform.position.x, posY + transform.position.y, 0);
+        _nav.SetDestination(destination);
+
+
+
+        // out of bounds check
+
     }
 }
