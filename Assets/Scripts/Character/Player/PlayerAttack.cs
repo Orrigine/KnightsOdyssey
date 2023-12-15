0cc305765582c2102b46a9fe898d090a8dd8c407
Hazmat11 <92298810+Hazmat11@users.noreply.github.com>
2023-12-15 16:00:44 +0100

    Boss huge attack and healing

--- Assets/Scripts/Character/Player/PlayerAttack.cs
+++ Assets/Scripts/Character/Player/PlayerAttack.cs
@@ -6,12 +6,21 @@ using UnityEngine.AI;
 public class PlayerAttack : MonoBehaviour
 {
     [SerializeField] GameObject _attackHitbox;
+    [SerializeField] GameObject _hugeAttack;
     
     private GameObject _attackHitboxInstance;
 
     public void Attack(Vector3 position, Quaternion rotation)
     {
         _attackHitboxInstance = Instantiate(_attackHitbox);
+        _attackHitboxInstance.GetComponent<Transform>().parent = gameObject.GetComponent<Transform>();
+        _attackHitboxInstance.GetComponent<Transform>().position = position;
+        _attackHitboxInstance.GetComponent<Transform>().rotation = rotation;
+    }
+
+    public void HugeAttack(Vector3 position, Quaternion rotation)
+    {
+        _attackHitboxInstance = Instantiate(_hugeAttack);
         _attackHitboxInstance.GetComponent<Transform>().parent = gameObject.GetComponent<Transform>();
         _attackHitboxInstance.GetComponent<Transform>().position = position;
         _attackHitboxInstance.GetComponent<Transform>().rotation = rotation;
