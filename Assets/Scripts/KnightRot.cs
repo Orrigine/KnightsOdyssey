327feb10e7bf8946137283373f00f83c3f979643
Valentin Faguet <valentin@wallmatt.com>
2023-12-18 11:02:29 +0100

    :star: Start Animate enemies

--- Assets/Scripts/KnightRot.cs
+++ Assets/Scripts/KnightRot.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class KnightRot : MonoBehaviour
 {
+
+    [SerializeField] public GameObject knight;
+    [SerializeField] public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        knight = GetComponent<GameObject>();
     }
 
     // Update is called once per frame
